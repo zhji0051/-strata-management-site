876fe35d85d4a0e5d65f9556e2d70718bb8c99e0
zhji0051 <zhji0051@uni.sydney.edu.au>
2025-03-28 03:37:19 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to the Strata Property Management System</h1>
+        <p>Here you can manage apartment property affairs</p>
+      </div>
+    );
+  }
